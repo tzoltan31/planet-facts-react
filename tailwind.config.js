9848ac0130ce5e0ab56c6687f4298a43b1df8cbd
Zoltán <tothz25@gmail.com>
2021-07-16 00:18:05 +0200

    safe purge options

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,44 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  purge: {
+    options: [
+      "bg-mercury",
+      "bg-venus",
+      "bg-earth",
+      "bg-mars",
+      "bg-jupiter",
+      "bg-saturn",
+      "bg-uranus",
+      "bg-neptune",
+      "bg-background",
+      "bg-greydark",
+      "bg-greylight",
+
+      "border-mercury",
+      "border-venus",
+      "border-earth",
+      "border-mars",
+      "border-jupiter",
+      "border-saturn",
+      "border-uranus",
+      "border-neptune",
+      "border-background",
+      "border-greydark",
+      "border-greylight",
+
+      "text-mercury",
+      "text-venus",
+      "text-earth",
+      "text-mars",
+      "text-jupiter",
+      "text-saturn",
+      "text-uranus",
+      "text-neptune",
+      "text-background",
+      "text-greydark",
+      "text-greylight",
+    ],
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
